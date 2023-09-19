098e7d6855578de39fe418c476f3d605cf8ea5e5
AvihayR <avihayrabi@gmail.com>
2023-09-19 20:23:48 +0300

    Change header size & logo content

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,7 +4,7 @@ export function AppHeader() {
     return (
         <header className="app-header">
             <Link to="/">
-                <h3>TC</h3>
+                <h3>ContactX</h3>
             </Link>
             <nav>
                 <NavLink to="/">Home</NavLink>
