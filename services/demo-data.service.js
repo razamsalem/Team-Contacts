f606eaba38857c92d5bc8f7c60b22976e64f4482
Raz <amsalemraz10@gmail.com>
2023-09-19 16:51:37 +0300

    F/add-demo-data/ add demo data to the app

---
+++ services/demo-data.service.js
@@ -0,0 +1,37 @@
+const demoData = [
+    {
+        _id: 't100',
+        firstName: 'Clara',
+        lastName: 'Wyatt',
+        email: 'claraw@gmail.com',
+        phone: '563-554-1703',
+        desc: 'Living in usa'
+    },
+    {
+        _id: 't101',
+        firstName: 'Virginia',
+        lastName: ' Carr',
+        email: 'virginia@gmail.com',
+        phone: '503-554-1783',
+        desc: 'virgina live in virgina usa'
+    },
+    {
+        _id: 't102',
+        firstName: 'Ofir',
+        lastName: 'Berlin',
+        email: 'ofir1245@gmail.com',
+        phone: '0806064116',
+        desc: 'work at Tel aviv'
+    },
+    {
+        _id: 't103',
+        firstName: 'Celine',
+        lastName: 'heler',
+        email: 'Celine@gmail.com',
+        phone: '055-5469571',
+        desc: 'celine love to go to pubs at nights'
+    },
+
+]
+
+export default demoData;
